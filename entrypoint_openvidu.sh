bd6770b21ff50935bcfea1da234057ae1416f59b
Nikita Konev <nikit.cpp@yandex.ru>
2021-01-14 03:16:58 +0300

    add exec to java

--- entrypoint_openvidu.sh
+++ entrypoint_openvidu.sh
@@ -24,4 +24,4 @@ if [ ! -z "${JAVA_OPTIONS}" ]; then
     printf "\n  Using java options: %s" "${JAVA_OPTIONS}"
 fi
 
-java ${JAVA_OPTIONS:-} -jar openvidu-server.jar
+exec java ${JAVA_OPTIONS:-} -jar openvidu-server.jar
